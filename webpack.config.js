22eb01dde5861bdada138feaafd70ec47d008ad3
SvetaPanin <spanin.sp@gmail.com>
2019-03-07 17:45:52 -0500

    last updates

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
+    entry: './src/index.ts',
     module: {
-
         rules: [
             {
                 test: /\.tsx?$/,
